36db73355c5c9a207a3bf6a776189eb6cebdb4f4
diptowebhero <diptodas1672@gmail.com>
2022-05-17 20:29:16 +0600

    added delete functionality

--- js/app.js
+++ js/app.js
@@ -27,23 +27,23 @@ form.addEventListener("submit", function (e) {
     formData.id = uuidv4();
     formData.status = "incomplete";
     const tasks = getDataFormLocalStorage();
-    tasks.push(formData)
-    displayingData(formData,tasks.length);
-    setDataOnLocalStorage(tasks)
+    tasks.push(formData);
+    displayingData(formData, tasks.length);
+    setDataOnLocalStorage(tasks);
   }
   this.reset();
 });
 
 //displaying data in the ui
-function displayingData({ name, priority, date, id, status },index) {
+function displayingData({ name, priority, date, id, status }, index) {
   const tr = document.createElement("tr");
   tr.innerHTML = `
   <td>${index}</td>
-            <td>${name}</td>
-            <td>${priority}</td>
-            <td>${status}</td>
-            <td>${date}</td>
-            <td>
+            <td id="name">${name}</td>
+            <td id="priority">${priority}</td>
+            <td id="status">${status}</td>
+            <td id="date">${date}</td>
+            <td id="action">
               <button id="edit">
                 <i class="fa-solid fa-pen-to-square"></i>
               </button>
@@ -57,27 +57,49 @@ function displayingData({ name, priority, date, id, status },index) {
   tBody.appendChild(tr);
 }
 
+//remove,update,delete,functionality
+tBody.addEventListener("click", function (e) {
+  if (e.target.id === "delete") {
+    const tr = e.target.parentElement.parentElement;
+    const id = tr.dataset.id;
+    tr.remove();
+    let tasks = getDataFormLocalStorage();
+    tasks = tasks.filter((task) => {
+      if (task.id !== id) {
+        return task;
+      }
+    });
+    
+    setDataOnLocalStorage(tasks);
+    load();
+  } else if (e.target.id === "check") {
+    console.log("check");
+  } else if (e.target.id === "edit") {
+    console.log("edit");
+  }
+});
 
 //onload function
 window.onload = load;
 
-function load(){
+function load() {
+  tBody.innerHTML = '';
   const tasks = getDataFormLocalStorage();
-  tasks.forEach((task,index)=>{
-    displayingData(task,index + 1)
-  })
+  tasks.forEach((task, index) => {
+    displayingData(task, index + 1);
+  });
 }
 
 //local storage function
-function getDataFormLocalStorage(){
+function getDataFormLocalStorage() {
   let task = [];
-  const data = localStorage.getItem('tasks');
-  if(data){
-    task = JSON.parse(data)
+  const data = localStorage.getItem("tasks");
+  if (data) {
+    task = JSON.parse(data);
   }
   return task;
 }
 
-function setDataOnLocalStorage(task){
-  localStorage.setItem('tasks',JSON.stringify(task))
+function setDataOnLocalStorage(task) {
+  localStorage.setItem("tasks", JSON.stringify(task));
 }
